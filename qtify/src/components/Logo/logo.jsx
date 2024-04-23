264ff4ea5c9fc01e15dd3c56717a02161b6b62c8
Roopa S <roopa31113@gmail.com>
2024-04-23 15:58:19 +0530

    C3:Button

--- qtify/src/components/Logo/logo.jsx
+++ qtify/src/components/Logo/logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import LogoImage from "../../assests/logo.png";
+import styles from "./logo.module.css";
 
 
 export default function logo(){
     return (
-        <img src={LogoImage} alt="Logo"/>
+        <img src={LogoImage} alt="Logo"vclassName={styles.logo}/>
     );
 }
